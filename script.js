bc172b96263b6056b78d2f4f02dd35980fc89c0b
Xiaoxin-Chen-1994 <49115976+Xiaoxin-Chen-1994@users.noreply.github.com>
2026-05-30 23:16:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -7133,8 +7133,8 @@ function closeSelector() {
 
   stopBackspaceHold();
 
-  // // Clear dummy state so further back presses exit normally
-  // history.back();
+  // Clear dummy state so further back presses exit normally
+  history.back();
 }
 window.closeSelector = closeSelector;
 
