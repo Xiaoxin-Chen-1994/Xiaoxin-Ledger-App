66ab9a24ade1f4e8b50328a0c3f5fcee03b6c2c0
Xiaoxin-Chen-1994 <49115976+Xiaoxin-Chen-1994@users.noreply.github.com>
2026-09-13 14:20:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -446,7 +446,7 @@ if (isMobileBrowser()) {
   // Helper function to set mobile status bar color
   const updateMobileStatusBar = () => {
     const isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-    const newColor = isDark ? "#000000" : "#FFFFFF";
+    const newColor = isDark ? "black" : "white";
     alert("isDark " + isDark + " newColor " + newColor);
     let metaThemeColor = document.querySelector("meta[name=theme-color]");
     if (metaThemeColor) {
